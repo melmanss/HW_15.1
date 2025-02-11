5312c4701c3fa8618d750dfe3c533413e47fec76
melmanss <cfhfyjdbx@gmail.com>
2025-02-11 09:32:02 +0200

    NEW

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        DayAdvisor advisor = new DayAdvisor();
+        System.out.print("Введіть день тижня (MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY): ");
+        String input = scanner.nextLine().toUpperCase();
+        try {
+            Day day = Day.valueOf(input);
+            advisor.advise(day);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Невірний ввід. Будь ласка, введіть день тижня на англійській мові.");
         }
+        scanner.close();
     }
 }
